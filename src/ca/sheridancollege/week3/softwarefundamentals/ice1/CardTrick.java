15234c4ad6c9d5ae79889531484adb9536902901
DigvijaySingh <cooldigvijay888@gmail.com>
2021-06-03 04:30:20 +0530

    Digvijay Singh Sheridan ID: 991638270

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -4,6 +4,12 @@
  * and open the template in the editor.
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
+/** 
+ * username: sindigvi
+ * firstname: digvijay singh 
+ * this is my branch file - I have completed the code to create 7 random files
+ */
+import java.util.Random;
 
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
@@ -12,21 +18,43 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * @author Sivagama 
  */
 public class CardTrick {
-    
-    public static void main(String[] args)
-    {
-        Card[] magicHand = new Card[7];
-        
-        for (int i=0; i<magicHand.length; i++)
-        {
-            Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-        }
-        
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
-    }
-    
+  
+   public static void main(String[] args) {
+
+       Card[] magicHand = new Card[7];
+       Random ran = new Random();
+       for(int i=0;i<magicHand.length;i++)
+       {
+           Card c = new Card();
+          
+           c.setValue(ran.nextInt(13)+1); // randomly generate a value between [1,13]
+           c.setSuit(Card.SUITS[ran.nextInt(4)]); // randomly generate a number between [0,3] and get the String stores in SUITS array in that index
+           magicHand[i] = c; // set ith card of magicHand to c
+       }
+      
+       boolean found = false;
+       // create a luckyCard
+       Card luckyCard = new Card();
+       luckyCard.setValue(9);
+       luckyCard.setSuit(Card.SUITS[2]);
+      
+       // loop to determine if luckyCard is present in magicHand
+       for(int i=0;i<magicHand.length;i++)
+       {
+           if(magicHand[i].getValue() == luckyCard.getValue() && magicHand[i].getSuit().equalsIgnoreCase(luckyCard.getSuit()))
+           {
+               found = true;
+               break;
+           }
+       }
+      
+       // display the result based on the search
+       if(found)
+           System.out.println("Lucky card found in magicHand");
+       else
+           System.out.println("Lucky card not found in magicHand");
+   }
+
 }
+
+//end of CardTrick.java
